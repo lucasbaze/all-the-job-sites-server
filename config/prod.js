b02e1b1e5b1b820ebf3f1dc065f5887e41505a90
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-02 20:30:01 -0500

    fixing firestore

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
     firestoreProjectID: process.env.FIRESTORE_PROJECT_ID,
-    firestoreKeys: process.env.FIRESTORE_KEYS,
+    firestoreKeys: process.env.FIREBASE_KEYS,
     firestoreClientEmail: process.env.FIRESTORE_CLIENT_EMAIL,
     firestorePrivateKey: process.env.FIRESTORE_PRIVATE_KEY,
     firestoreDB: process.env.FIRESTORE_DB,
