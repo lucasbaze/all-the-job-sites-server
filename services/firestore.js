ac03cd79f875887efc18dcb7c0634eb3342402f4
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-02 19:05:46 -0500

    parse keys

--- services/firestore.js
+++ services/firestore.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 const keys = require('../config/keys');
 
 admin.initializeApp({
-    credential: admin.credential.cert(keys.firestoreKey),
+    credential: admin.credential.cert(JSON.parse(keys.firestoreKey)),
     databaseURL: keys.firestoreDB,
 });
 
